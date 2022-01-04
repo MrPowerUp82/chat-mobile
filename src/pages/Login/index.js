7133909abe975376542d597ea7569c17e2edbb18
MrPowerUp82 <gustavohenrique8282@hotmail.com>
2022-01-04 13:26:26 -0300

    starting dynamic app

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Container, InputContainer,Title,Input, Button, ButtonTitle} from './styles';
 
 export default function Login(){
+
+    const [inputUsername,setInputUsername] = useState('')
+
+    async function handleLogin(){
+
+    }
+
     return(
         <Container>
             <InputContainer>
                 <Title>Login Page</Title>
-                <Input placeholder="Username"/>
+                <Input placeholder="Username" value={inputUsername} onChange={(text)=>setInputUsername(text)}/>
                 <Input placeholder="Password"/>
                 <Button>
                     <ButtonTitle>LOGIN</ButtonTitle>
